4cc08cb5f17155d2b7cea5186dcff39d1a9b5dec
NG Code Lab <ngcodelab@yahoo.com>
2016-06-01 16:20:31 +0000

    Added active-model-serializers gem. Added api namespace routing block.

--- Gemfile
+++ Gemfile
@@ -9,9 +9,10 @@ gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
-
 gem 'devise'
 
+# Gems for API
+gem 'active_model_serializers'
 
 group :production do
   gem 'pg'
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   
   devise_for :users
+  
+  # Routes for API
+  namespace :api, defaults: { format: :json }, path: '/'  do
+
+  end
+  
+  
+  
   root 'application#index'
-  
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
