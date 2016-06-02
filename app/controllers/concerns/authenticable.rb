ee69571e190c087b662183fa844fec514c5297b9
NG Code Lab <ngcodelab@yahoo.com>
2016-06-02 02:48:47 +0000

    Adds the authenticate with token method to handle access to actions

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -1,5 +1,14 @@
 module Authenticable
+  
+  # Devise methods overrides
   def current_user
     @current_user || User.find_by(auth_token: request.headers['Authorization'])
   end
+  
+  def authenticate_with_token!
+    unless current_user.present?
+      render json: { errors: "Not autheniticated" }, status: :unauthorized
+    end
+  end
+  
 end
