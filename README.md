3c703dd007e7481b4c5a7774877270faeae3a9c3
NG Code Lab <ngcodelab@yahoo.com>
2016-06-02 02:20:37 +0000

    added auth_token field to user model. Generate unique auth_token for the user on creation

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 
 GET: https://api-on-rails-app-ngcodelab.c9users.io/api/users/1.json
+
 POST: https://api-on-rails-app-ngcodelab.c9users.io/api/users.json
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,21 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  before_create :generate_unique_auth_token!
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :auth_token, uniqueness: true
+  
+  
+  
+  # Generate a unique auth_token. This method randomly generates a token
+  # and then checks for a record which has already taken the new token.
+  # If there exists a record with the same token, then another token 
+  # is generated and the process continues unless a uniq token is found.
+  def generate_unique_auth_token!
+    begin
+      self.auth_token = Devise.friendly_token
+    end while self.class.exists?(auth_token: auth_token)
+  end  
 end
